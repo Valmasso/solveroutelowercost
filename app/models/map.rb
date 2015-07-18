5c5db56127c7f7d6834eeaea2dc5465e7747156f
Valmir Dimas <valmirdimas@gmail.com>
2015-07-18 02:07:17 -0300

    Add methods and spec

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,5 @@
+require_relative '../../lib/graph'
+
 class Map < ActiveRecord::Base
   has_many :paths, dependent: :destroy
 
@@ -5,5 +7,23 @@ class Map < ActiveRecord::Base
 
   with_options allow_blank: true do |v|
     v.validates :name, uniqueness: true
+  end
+
+  def to_graph
+    paths.map { |p| [p.from, p.to, p.distance.to_f] }
+  end
+
+  def to_show
+    to_graph.map(&:to_s).join("\n")
+  end
+
+  def search_path(path, fuel)
+    graph = Graph.new(to_graph)
+    start, stop = [path.from, path.to]
+    found_path, dist = graph.shortest_path(start, stop)
+    autonomy = path.distance
+    distance = dist
+    cost = (distance / autonomy) * fuel
+    [found_path, cost]
   end
 end
--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -27,4 +27,47 @@ describe Map, type: :model do
       end
     end
   end
+
+  describe "instance methods" do
+    describe "#search_path" do
+      let(:map) { create(:map_with_paths_defined) }
+      let(:path) { build(:path, from: "A", to: "D", distance: 10) }
+      let(:fuel) { 2.5 }
+
+      it "result with 2 elements" do
+        expect(map.search_path(path, fuel).size).to eq(2)
+        expect(map.search_path(path, fuel)).to eq([["A", "B", "D"], 6.25])
+      end
+    end
+
+    describe "#to_graph" do
+      let(:map) { create(:map_with_paths_defined) }
+      let(:result) do
+        [
+          ["D", "E", 30.0],
+          ["A", "C", 20.0],
+          ["C", "D", 30.0],
+          ["A", "B", 10.0],
+          ["B", "E", 50.0],
+          ["B", "D", 15.0]
+        ]
+      end
+
+      it "result with 6 elements" do
+        expect(map.to_graph.size).to eq(6)
+        expect(map.to_graph).to eq(result)
+      end
+    end
+
+    describe "#to_show" do
+      let(:map) { create(:map_with_paths_defined) }
+      let(:result) do
+        "[\"D\", \"E\", 30.0]\n[\"A\", \"C\", 20.0]\n[\"C\", \"D\", 30.0]\n[\"A\", \"B\", 10.0]\n[\"B\", \"E\", 50.0]\n[\"B\", \"D\", 15.0]"
+      end
+
+      it "result with success" do
+        expect(map.to_show).to eq(result)
+      end
+    end
+  end
 end
